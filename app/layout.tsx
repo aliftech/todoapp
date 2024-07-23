a5fc8335e95d5872c9f67883bbd32d4c3fb92901
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2024-07-23 15:32:59 +0700

    refactor: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="next.svg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
